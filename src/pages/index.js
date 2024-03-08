2993c5f50707f2a4fe6833041183772a5b4653d7
Paraskevas Leivadaros <paraskevasleivadaros@gmail.com>
2024-03-09 00:29:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,13 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        // <div className={styles.buttons}>
-        //   <Link
-        //     className="button button--secondary button--lg"
-        //     to="/docs/intro">
-        //     Docusaurus Tutorial - 5min ⏱️
-        //   </Link>
-        // </div>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
       </div>
     </header>
   );
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
