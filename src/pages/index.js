ad8e26b52a856dc4ee36331c0b055ca7f81bb2bd
Paraskevas Leivadaros <paraskevasleivadaros@gmail.com>
2024-03-09 00:31:40 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://youtube.com/@theatrikiopa">
+            Παραστάσεις 🎭
           </Link>
         </div>
       </div>
