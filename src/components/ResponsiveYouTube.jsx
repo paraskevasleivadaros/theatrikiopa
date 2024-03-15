227d8b02bd98274b13afeba54fe9b8ee189b03b6
Paraskevas Leivadaros <paraskevasleivadaros@gmail.com>
2024-03-15 20:30:00 +0200

    Create ResponsiveYouTube.jsx

---
+++ src/components/ResponsiveYouTube.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ResponsiveYouTube = ({ videoId }) => {
+  const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div style={{
+      position: 'relative',
+      paddingBottom: '56.25%', // 16:9 Aspect Ratio
+      paddingTop: '25px',
+      height: 0
+    }}>
+      <iframe 
+        src={videoUrl}
+        title="YouTube video player"
+        frameborder="0"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+        }}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default ResponsiveYouTube;
