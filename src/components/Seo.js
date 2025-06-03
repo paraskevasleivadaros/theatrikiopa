a466d1313356e08251fc8c70b47da6804a02934d
Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>
2025-06-02 22:40:11 -0400

    Update Seo.js
    Signed-off-by: Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-export default function Seo({ title, description, image, url }) {
+export default function Seo({ title, description, image, imageAlt, url }) {
   const { siteConfig } = useDocusaurusContext();
   const siteTitle = title || siteConfig.title;
   const siteDesc = description || siteConfig.tagline;
@@ -13,16 +13,25 @@ export default function Seo({ title, description, image, url }) {
     ? image
     : `${baseUrl.replace(/\/$/, '')}/${image?.replace(/^\/+/, '') || 'img/logo.png'}`;
 
-  const siteUrl = url || baseUrl;
+  const resolvedImage = image?.startsWith('http')
+    ? image
+    : `${siteConfig.url}/${image?.replace(/^\/+/, '')}`; // ensure no leading slashes
+  
+  const siteUrl = url || siteConfig.url;
 
   return (
     <Head>
       <meta property="og:title" content={siteTitle} />
       <meta property="og:description" content={siteDesc} />
-      <meta property="og:image" content={siteImage} />
+      <meta property="og:image" content={resolvedImage} />
+      {imageAlt && <meta property="og:image:alt" content={imageAlt} />}
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
+
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={resolvedImage} />
     </Head>
   );
 }
