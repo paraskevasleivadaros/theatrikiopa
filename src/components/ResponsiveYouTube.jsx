24aa4b07f41012e3403f66321c02263f0593b38d
Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>
2025-06-05 10:52:13 -0400

    Update ResponsiveYouTube.jsx
    Signed-off-by: Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>

--- src/components/ResponsiveYouTube.jsx
+++ src/components/ResponsiveYouTube.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const ResponsiveYouTube = ({ videoId }) => {
-  const videoUrl = `https://youtube.com/embed/${videoId}`;
+  const isShort = videoId.startsWith('shorts/');
+  const embedId = isShort ? videoId.replace('shorts/', '') : videoId;
+  const videoUrl = `https://www.youtube.com/embed/${embedId}`;
 
   return (
     <div style={{
@@ -9,12 +11,12 @@ const ResponsiveYouTube = ({ videoId }) => {
       paddingBottom: '56.25%', // 16:9 Aspect Ratio
       paddingTop: '25px',
       height: 0,
-      marginBottom: '20px' // Add 20px of space at the bottom
+      marginBottom: '20px'
     }}>
-      <iframe 
+      <iframe
         src={videoUrl}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         style={{
           position: 'absolute',
           top: 0,
@@ -23,7 +25,7 @@ const ResponsiveYouTube = ({ videoId }) => {
           height: '100%'
         }}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
