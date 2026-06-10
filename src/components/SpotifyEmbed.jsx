001e615cd84b6d6e7b8c7ef0357634fa09c6b4e9
Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>
2026-06-11 00:47:35 +0200

    Update SpotifyEmbed.jsx
    Signed-off-by: Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>

--- src/components/SpotifyEmbed.jsx
+++ src/components/SpotifyEmbed.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 
 const SpotifyEmbed = ({ src }) => {
+  const getEmbedUrl = (url) => {
+    if (!url) return '';
+
+    return url
+      .replace('open.spotify.com/', 'open.spotify.com/embed/')
+      .split('?')[0]; // removes ?si=... tracking
+  };
+
+  const embedSrc = getEmbedUrl(src);
+
   return (
     <div
       style={{
         position: 'relative',
         width: '100%',
         maxWidth: '400px',
-        height: '152px', // Spotify’s standard embed height for tracks
+        height: '352px', // better for albums/playlists
         margin: '0 0 20px 0',
         borderRadius: '12px',
         overflow: 'hidden',
       }}
     >
       <iframe
-        src={src}
+        src={embedSrc}
         style={{
           position: 'absolute',
           top: 0,
           left: 0,
           width: '100%',
           height: '100%',
-          border: '0',
+          border: 0,
         }}
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
