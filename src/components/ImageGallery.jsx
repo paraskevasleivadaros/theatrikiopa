ea7bfda697ef021d356fc0362a77d5676e208f57
Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>
2026-06-12 21:31:34 +0200

    Update ImageGallery.jsx
    Signed-off-by: Paraskevas K. Leivadaros <paraskevasleivadaros@gmail.com>

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styles from './styles.module.css';
 
 export default function ImageGallery({ images = [] }) {
   const [selectedImage, setSelectedImage] = useState(null);
